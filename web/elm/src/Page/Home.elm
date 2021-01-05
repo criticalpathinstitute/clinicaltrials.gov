f703a4e7dac26702149d3693708cc89f854ca0a4
Ken Youens-Clark <kyclark@gmail.com>
2021-01-05 14:07:13 -0700

    adding conditions

--- web/elm/src/Page/Home.elm
+++ web/elm/src/Page/Home.elm
@@ -2,7 +2,9 @@ module Page.Home exposing (Model, Msg, init, subscriptions, update, view)
 
 import Bootstrap.Button as Button
 import Bootstrap.Form as Form
+import Bootstrap.Form.Checkbox as Checkbox
 import Bootstrap.Form.Input as Input
+import Bootstrap.Form.Select as Select
 import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Col as Col
 import Bootstrap.Grid.Row as Row
@@ -11,11 +13,12 @@ import Common exposing (commify, viewHttpErrorMessage)
 import Config exposing (apiServer)
 import Debug
 import Html exposing (Html, a, div, h1, text)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (class, for, value)
 import Html.Events exposing (onInput, onSubmit)
 import Http
 import Json.Decode exposing (Decoder, field, float, int, nullable, string)
 import Json.Decode.Pipeline exposing (hardcoded, optional, required)
+import Regex
 import RemoteData exposing (RemoteData, WebData)
 import Route
 import Url.Builder
@@ -24,7 +27,17 @@ import Url.Builder
 type alias Model =
     { summary : WebData Summary
     , query : Maybe String
+    , selectedConditions : List String
+    , conditionFilter : Maybe String
+    , conditionsDropDown : WebData (List ConditionDropDown)
     , searchResults : WebData (List Study)
+    }
+
+
+type alias ConditionDropDown =
+    { conditionId : Int
+    , condition : String
+    , num_studies : Int
     }
 
 
@@ -40,7 +53,11 @@ type alias Study =
 
 
 type Msg
-    = SummaryResponse (WebData Summary)
+    = ConditionDropDownResponse (WebData (List ConditionDropDown))
+    | RemoveCondition String
+    | SummaryResponse (WebData Summary)
+    | SetCondition String
+    | SetConditionFilter String
     | SetQuery String
     | SearchResponse (WebData (List Study))
     | DoSearch
@@ -50,17 +67,34 @@ init : ( Model, Cmd Msg )
 init =
     ( { summary = RemoteData.NotAsked
       , query = Nothing
+      , selectedConditions = []
+      , conditionFilter = Nothing
+      , conditionsDropDown = RemoteData.NotAsked
       , searchResults = RemoteData.NotAsked
       }
-    , Cmd.batch [ getSummary ]
+    , Cmd.batch [ getSummary, getConditionDropDown ]
     )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        ConditionDropDownResponse data ->
+            ( { model | conditionsDropDown = data }
+            , Cmd.none
+            )
+
         DoSearch ->
-            ( model, doSearch model.query )
+            ( model, doSearch model )
+
+        RemoveCondition condition ->
+            let
+                newConditions =
+                    List.filter
+                        (\c -> c /= condition)
+                        model.selectedConditions
+            in
+            ( { model | selectedConditions = newConditions }, Cmd.none )
 
         SummaryResponse data ->
             ( { model | summary = data }
@@ -69,6 +103,36 @@ update msg model =
 
         SearchResponse data ->
             ( { model | searchResults = data }
+            , Cmd.none
+            )
+
+        SetCondition condition ->
+            let
+                newCondition =
+                    case String.length condition of
+                        0 ->
+                            []
+
+                        _ ->
+                            [ condition ]
+            in
+            ( { model
+                | selectedConditions = model.selectedConditions ++ newCondition
+              }
+            , Cmd.none
+            )
+
+        SetConditionFilter text ->
+            let
+                newFilter =
+                    case String.length text of
+                        0 ->
+                            Nothing
+
+                        _ ->
+                            Just (String.toLower text)
+            in
+            ( { model | conditionFilter = newFilter }
             , Cmd.none
             )
 
@@ -102,24 +166,96 @@ view model =
                     viewHttpErrorMessage httpError
 
                 RemoteData.Success data ->
-                    "Search " ++ commify data.num_studies ++ " studies:"
+                    "Search " ++ commify data.num_studies ++ " studies"
 
-        hasQuery =
-            case model.query of
-                Just qry ->
-                    String.length qry > 0
+        mkConditionSelect =
+            let
+                empty =
+                    [ Select.item [ value "" ] [ text "--Select--" ] ]
+
+                mkSelectItem condition =
+                    Select.item [ value condition.condition ]
+                        [ text <|
+                            condition.condition
+                                ++ " ("
+                                ++ String.fromInt condition.num_studies
+                                ++ ")"
+                        ]
+
+                filterConditions conditions =
+                    let
+                        regex =
+                            case model.conditionFilter of
+                                Just filter ->
+                                    Just
+                                        (Maybe.withDefault Regex.never
+                                            (Regex.fromString filter)
+                                        )
+
+                                _ ->
+                                    Nothing
+                    in
+                    case regex of
+                        Just re ->
+                            List.filter
+                                (\c ->
+                                    Regex.contains re
+                                        (String.toLower c.condition)
+                                )
+                                conditions
+
+                        _ ->
+                            []
+
+                mkSelect data =
+                    case List.length data of
+                        0 ->
+                            text ""
+
+                        _ ->
+                            Select.select
+                                [ Select.id "condition", Select.onChange SetCondition ]
+                                (empty ++ List.map mkSelectItem data)
+            in
+            case model.conditionsDropDown of
+                RemoteData.Success data ->
+                    mkSelect (filterConditions data)
+
+                RemoteData.Failure httpError ->
+                    text (viewHttpErrorMessage httpError)
 
                 _ ->
-                    False
+                    text "Error fetching conditions"
+
+        viewCondition condition =
+            Button.button
+                [ Button.outlinePrimary
+                , Button.onClick (RemoveCondition condition)
+                ]
+                [ text (condition ++ " [X]") ]
+
+        viewSelectedConditions =
+            List.map viewCondition model.selectedConditions
 
         searchForm =
-            Form.formInline [ onSubmit DoSearch ]
+            Form.form [ onSubmit DoSearch ]
                 [ Form.label [] [ text summary ]
-                , Input.text
-                    [ Input.attrs [ onInput SetQuery ] ]
+                , Form.group []
+                    [ Form.label [ for "text" ] [ text "Text" ]
+                    , Input.text
+                        [ Input.attrs [ onInput SetQuery ] ]
+                    ]
+                , Form.group []
+                    ([ Form.label [ for "condition" ]
+                        [ text "Condition" ]
+                     , Input.text
+                        [ Input.attrs [ onInput SetConditionFilter ] ]
+                     , mkConditionSelect
+                     ]
+                        ++ viewSelectedConditions
+                    )
                 , Button.submitButton
                     [ Button.primary
-                    , Button.disabled (not hasQuery)
                     , Button.attrs [ class "ml-sm-2 my-2" ]
                     ]
                     [ text "Go" ]
@@ -189,16 +325,63 @@ getSummary =
         }
 
 
-doSearch : Maybe String -> Cmd Msg
-doSearch query =
-    case query of
-        Just term ->
-            let
-                url =
-                    apiServer ++ "/search/" ++ term
-            in
+getConditionDropDown : Cmd Msg
+getConditionDropDown =
+    let
+        url =
+            apiServer ++ "/conditions"
+    in
+    Http.get
+        { url = url
+        , expect =
+            Http.expectJson
+                (RemoteData.fromResult >> ConditionDropDownResponse)
+                (Json.Decode.list decoderConditionDropDown)
+        }
+
+
+doSearch : Model -> Cmd Msg
+doSearch model =
+    let
+        builder ( label, value ) =
+            case value of
+                Just v ->
+                    Just (Url.Builder.string label v)
+
+                _ ->
+                    Nothing
+
+        conditions =
+            case List.length model.selectedConditions of
+                0 ->
+                    Nothing
+
+                _ ->
+                    Just (String.join "::" model.selectedConditions)
+
+        queryParams =
+            Url.Builder.toQuery <|
+                List.filterMap builder
+                    [ ( "text"
+                      , model.query
+                      )
+                    , ( "conditions"
+                      , conditions
+                      )
+                    ]
+
+        url =
+            case String.length queryParams of
+                0 ->
+                    Nothing
+
+                _ ->
+                    Just <| apiServer ++ "/search/" ++ queryParams
+    in
+    case url of
+        Just someUrl ->
             Http.get
-                { url = url
+                { url = someUrl
                 , expect =
                     Http.expectJson
                         (RemoteData.fromResult >> SearchResponse)
@@ -207,6 +390,14 @@ doSearch query =
 
         _ ->
             Cmd.none
+
+
+decoderConditionDropDown : Decoder ConditionDropDown
+decoderConditionDropDown =
+    Json.Decode.succeed ConditionDropDown
+        |> Json.Decode.Pipeline.required "condition_id" int
+        |> Json.Decode.Pipeline.required "condition" string
+        |> Json.Decode.Pipeline.required "num_studies" int
 
 
 decoderSummary : Decoder Summary
--- web/fastapi/main.py
+++ web/fastapi/main.py
@@ -38,6 +38,10 @@ app.add_middleware(
 class Condition(BaseModel):
     condition: str
 
+class ConditionDropDown(BaseModel):
+    condition_id: int
+    condition: str
+    num_studies: int
 
 class StudySearchResult(BaseModel):
     nct_id: str
@@ -56,14 +60,43 @@ class Summary(BaseModel):
 dbh = psycopg2.connect('dbname=ct user=kyclark password="g0p3rl!"')
 
 # --------------------------------------------------
-@app.get('/search/{term}', response_model=List[StudySearchResult])
-def search(term: str):
+def get_cur():
+    """ Get db cursor """
+
+    return dbh.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+
+# --------------------------------------------------
+@app.get('/search', response_model=List[StudySearchResult])
+def search(text: Optional[str] = '', conditions: Optional[str] = ''):
+    """ Search """
+    def f(rec):
+        return StudySearchResult(nct_id=rec['nct_id'],
+                                 title=rec['official_title'])
+
+    flds = ['nct_id', 'official_title']
+    proj = {fld: 1 for fld in flds}
+    qry = {}
+
+    if text:
+        qry['$text'] = { '$search': text }
+
+    if conditions:
+        qry['conditions'] = { '$in': conditions.split('::') }
+
+    if qry:
+        return list(map(f, mongo_db['ct'].find(qry, proj)))
+
+
+# --------------------------------------------------
+@app.get('/quick_search/{term}', response_model=List[StudySearchResult])
+def quick_search(term: str):
     """ Search text for keyword """
     def f(rec):
         return StudySearchResult(nct_id=rec['nct_id'],
                                  title=rec['official_title'])
 
-    cur = dbh.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur = get_cur()
     sql = f"""
         select nct_id, official_title as title
         from   study
@@ -82,7 +115,12 @@ def search(term: str):
 def summary():
     """ DB summary stats """
 
-    return Summary(num_studies=mongo_db['ct'].find({}).count())
+    cur = get_cur()
+    cur.execute('select count(study_id) as num_studies from study')
+    res = cur.fetchone()
+    cur.close()
+
+    return Summary(num_studies=res['num_studies'])
 
 
 # --------------------------------------------------
@@ -90,20 +128,31 @@ def summary():
 def study(nct_id: str) -> StudyDetail:
     """ Study details """
 
-    if studies := list(Study.select().where(Study.nct_id == nct_id)):
-        study = studies[0]
+    if study := Study.query().where(Study.nct_id == nct_id):
         return StudyDetail(nct_id=study.nct_id,
                            title=study.official_title)
 
 
 # --------------------------------------------------
-@app.get('/conditions', response_model=List[Condition])
-def study(nct_id: str) -> StudyDetail:
-    """ Study details """
+@app.get('/conditions', response_model=List[ConditionDropDown])
+def conditions(name: Optional[str] = '') -> List[ConditionDropDown]:
+    """ Conditions/Num Studies """
 
-    res = mongo_db['ct'].find({'nct_id': nct_id})
+    clause = f"and c.condition like '%{name}%'" if name else ''
+    sql = f"""
+        select   c.condition_id, c.condition, count(s.study_id) as num_studies
+        from     condition c, study_to_condition s2c, study s
+        where    c.condition_id=s2c.condition_id
+        and      s2c.study_id=s.study_id
+        {clause}
+        group by 1, 2
+        order by 2
+    """
 
-    if res.count() == 1:
-        study = res.next()
-        return StudyDetail(nct_id=study['nct_id'],
-                           title=study['official_title'])
+    cur = get_cur()
+    cur.execute(sql)
+    res = cur.fetchall()
+    conditions = list(map(lambda r: ConditionDropDown(**dict(r)), res))
+    cur.close()
+
+    return conditions
